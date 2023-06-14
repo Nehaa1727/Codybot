ba0290a5d7f42b81d5f971b866b6199d22b0571e
Neha Mundhra <135490586+Nehaa1727@users.noreply.github.com>
2023-06-14 10:10:31 +0530

    __init__.py

--- cody/__init__.py
+++ cody/__init__.py
@@ -7,4 +7,4 @@ openai_key ="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"
 api_id ="15334693"
 api_hash = "a70a6aee50a682f654b784b2e8bc19d7"
 bot_token = "6215146547:AAG8MegA5hucMsbKRjFU9ZiN7cKBUkiONe0"
-bot = TelegramClient("codybot",api_id, api_hash).start(bot_token=bot_token)
+bot = TelegramClient("xcodybot",api_id, api_hash).start(bot_token=bot_token)
